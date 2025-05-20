117f5b9174a24659120d93b2c55d2e64557be186
CAI Luc <lcai240207@gmail.com>
2025-05-20 12:06:18 +0200

    Resize JavaFX & Affichage

--- src/main/java/fr/iut/saeterraria/sae/HelloApplication.java
+++ src/main/java/fr/iut/saeterraria/sae/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("vueTerraria.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(fxmlLoader.load(),200,200);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
